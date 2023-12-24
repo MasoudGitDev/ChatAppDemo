afc88a96d44016ed35e901eab94bd37c07a08c00
MasoudSh <Msh.Git.Dev@gmail.com>
2023-12-24 21:30:21 +0330

    create : LoginByToken,Login,Register Command models and their Handlers

---
+++ Applications/Apps.Auth/Accounts/Commands/Models/LoginCModel.cs
@@ -0,0 +1,20 @@
+﻿using Apps.Auth.Accounts.Abstractions.Models;
+using MediatR;
+using Shared.Enums;
+using Shared.Models;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Apps.Auth.Accounts.Commands.Models;
+
+public record LoginCModel : ILoginModel ,IRequest<AccountResult>
+{
+    [Required]
+    [Length(3, 50)]
+    public string LoginName { get; set; } = string.Empty;
+
+    [Required]
+    [Length(8, 50)]
+    public string Password { get; set; } = string.Empty;
+    public LoginTypes LoginType { get; set; } = LoginTypes.ByUserName;
+}
---
+++ Applications/Apps.Auth/Accounts/Commands/Models/RegisterCModel.cs
@@ -0,0 +1,26 @@
+﻿using Apps.Auth.Accounts.Abstractions.Models;
+using MediatR;
+using Shared.Models;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Apps.Auth.Accounts.Commands.Models;
+
+public record RegisterCModel : IRegisterModel, IRequest<AccountResult>
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; init; } = string.Empty;
+
+    [Required]
+    [Length(3, 50)]
+    public string UserName { get; init; } = string.Empty;
+
+    [Required]
+    [Length(8, 50)]
+    public string Password { get; init; } = string.Empty;
+
+    [Required]
+    [Compare(nameof(Password))]
+    public string PasswordConfirmed { get; init; } = string.Empty;
+}
