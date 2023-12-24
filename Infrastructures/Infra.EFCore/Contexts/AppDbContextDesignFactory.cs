de5cf9a1d4545d7b7af8d303c0acd0a6cd8c1555
MasoudSh <Msh.Git.Dev@gmail.com>
2023-12-24 21:56:39 +0330

    create : AppDbContext

---
+++ Infrastructures/Infra.EFCore/Contexts/AppDbContextDesignFactory.cs
@@ -0,0 +1,15 @@
+﻿using Infra.EFCore.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Infra.EFCore.Contexts;  
+internal class AppDbContextDesignFactory : IDesignTimeDbContextFactory<AppDbContext> {
+
+    public AppDbContext CreateDbContext(string[] args) {
+        var optionBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        var path = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ChatAppDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        optionBuilder.UseSqlServer(path);
+        return new AppDbContext(optionBuilder.Options);
+    }
+}
