1f7ad8a662b0f2a2681530bfa14643361ccf8eaf
MasoudSh <Msh.Git.Dev@gmail.com>
2023-12-24 21:58:13 +0330

    create : JweTypes

---
+++ Infrastructures/Infra.EFCore.Auth/Constants/JweTypes.cs
@@ -0,0 +1,5 @@
+﻿namespace Infra.EfCore.Auth.Constants;  
+public class JweTypes {
+    public static readonly string UserIdentifier = "UserIdentifier";
+    public static readonly string TokenId = "TokenId";
+}
