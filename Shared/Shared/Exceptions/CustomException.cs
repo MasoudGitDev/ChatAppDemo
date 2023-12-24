315b0fcef6c515541087face72662747532d00e5
MasoudSh <Msh.Git.Dev@gmail.com>
2023-12-24 21:09:41 +0330

    create : CustomException to handle Exceptions by Where,Code and Description Props

---
+++ Shared/Shared/Exceptions/CustomException.cs
@@ -0,0 +1,13 @@
+﻿namespace Shared.Exceptions;  
+public class CustomException :Exception{
+    public string Where { get; } = String.Empty;
+    public string Code { get; } = String.Empty;
+    public string Description { get; } = String.Empty;
+
+    public CustomException(string where , string code , string description) :
+        base(message: $"Where : {where}{Environment.NewLine}Code : {code}{Environment.NewLine}Description : {description}{Environment.NewLine}") {
+        Where = where;
+        Code = code;
+        Description = description;
+    }
+}
