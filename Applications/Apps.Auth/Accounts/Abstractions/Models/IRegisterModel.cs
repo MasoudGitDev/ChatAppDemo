2cf474547221e822ab164f77572082c3c6d00712
MasoudSh <Msh.Git.Dev@gmail.com>
2023-12-24 21:29:13 +0330

    create : ILogin & IRegister  Interface Models

---
+++ Applications/Apps.Auth/Accounts/Abstractions/Models/IRegisterModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Apps.Auth.Accounts.Abstractions.Models;
+public interface IRegisterModel
+{
+    string Email { get; init; }
+    string UserName { get; init; }
+    string Password { get; init; }
+    string PasswordConfirmed { get; init; }
+}
