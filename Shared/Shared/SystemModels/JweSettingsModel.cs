bc35585c00e66392183e9beb26153ce49e59e67c
MasoudSh <Msh.Git.Dev@gmail.com>
2023-12-24 21:11:31 +0330

    create : JweSettingsModel

---
+++ Shared/Shared/SystemModels/JweSettingsModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Shared.SystemModels;
+public class JweSettingsModel {
+    public string Issuer { get; set; } = string.Empty;
+    public string Audience { get; set; } = string.Empty;
+    public byte ExpireAfterMinute { get; set; } = 0;
+    public string SecretKey { get; set; } = string.Empty;
+}
