ad49f0adbf45eda8a7a5a08a410b007716f6f32b
MasoudSh <Msh.Git.Dev@gmail.com>
2023-12-25 14:27:28 +0330

    update : resigester services and update imports

--- Presentations/Client.WebAssembly/Program.cs
+++ Presentations/Client.WebAssembly/Program.cs
@@ -1,11 +1,18 @@
+using Blazored.LocalStorage;
 using Client.WebAssembly;
+using Client.WebAssembly.Services;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7071") });
+builder.Services.AddScoped<IAccountService , AccountService>();
+builder.Services.AddScoped<AuthenticationStateProvider , AuthStateProvider>();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
