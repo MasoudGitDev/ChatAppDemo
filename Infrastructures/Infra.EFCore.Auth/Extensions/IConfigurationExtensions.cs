7194b45452014e1197b008b37cb396dd1cd51035
MasoudSh <Msh.Git.Dev@gmail.com>
2023-12-25 21:21:07 +0330

    update : names

--- Infrastructures/Infra.EFCore.Auth/Extensions/IConfigurationExtensions.cs
+++ Infrastructures/Infra.EFCore.Auth/Extensions/IConfigurationExtensions.cs
@@ -1,12 +1,14 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Infra.EfCore.Auth.Exceptions;
+using Microsoft.Extensions.Configuration;
+using Shared.Enums;
 using Shared.SystemModels;
 
 namespace Infra.EfCore.Auth.Extensions;
 public static class IConfigurationExtensions {
-    public static JweSettingsModel GetJwtSettings(this IConfiguration configuration) {
-        var jwtSettings = configuration.GetSection("JWTSettings").Get<JweSettingsModel>();
+    public static JweSettingsModel GetJweSettings(this IConfiguration configuration) {
+        var jwtSettings = configuration.GetSection(AppSettingNames.JweSettings).Get<JweSettingsModel>();
         if(jwtSettings is null) {
-            throw new ArgumentNullException("GetJwtSettings : <JwtSettingsModel> is null.");
+            throw new JweException(nameof(GetJweSettings) , "NullObj" , $"The <{nameof(JweSettingsModel)}> can not be null." );
         }
         return jwtSettings;
     }
