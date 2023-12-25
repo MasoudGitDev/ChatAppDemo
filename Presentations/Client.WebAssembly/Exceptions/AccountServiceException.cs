e31cb5975581362d7e24674d371cfbe662a8b9fa
MasoudSh <Msh.Git.Dev@gmail.com>
2023-12-25 14:24:12 +0330

    create : AccountService and AuthStateProvider Exceptions

---
+++ Presentations/Client.WebAssembly/Exceptions/AccountServiceException.cs
@@ -0,0 +1,7 @@
+﻿using Shared.Exceptions;
+
+namespace Client.WebAssembly.Exceptions;  
+public class AccountServiceException : CustomException {
+    public AccountServiceException(string where , string code , string description) : base(where , code , description) {
+    }
+}
