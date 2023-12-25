aa49883bc4304728ce62af92e299a36c500bb186
MasoudSh <Msh.Git.Dev@gmail.com>
2023-12-25 14:49:20 +0330

    create : AccountController

---
+++ Presentations/Server.WebAPI/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Apps.Auth.Accounts.Commands.Models;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Models;
+
+namespace Server.WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountController(ISender sender) : ControllerBase {
+
+    [HttpPost("Login")]
+    public async Task<AccountResult> LoginAsync(LoginCModel loginModel) {
+        return await sender.Send(loginModel);
+    }
+
+    [HttpPost("LoginByToken")]
+    public async Task<AccountResult> LoginByTokenAsync(LoginByTokenCModel loginByTokenModel) {
+        return await sender.Send(loginByTokenModel);
+    }
+
+    [HttpPost("Register")]
+    public async Task<AccountResult> RegisterAsync(RegisterCModel registerModel) {
+        return await sender.Send(registerModel);
+    }
+
+}
