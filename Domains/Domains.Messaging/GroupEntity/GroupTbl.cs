0cadc6289881f01faca83ce00968e8687f8da511
MasoudSh <Msh.Git.Dev@gmail.com>
2023-12-26 12:32:11 +0330

    create : GroupTbl Entity and Admin & Member ValueObjects

---
+++ Domains/Domains.Messaging/GroupEntity/GroupTbl.cs
@@ -0,0 +1,26 @@
+﻿using Domains.Auth.UserEntity;
+using Domains.Messaging.Shared.ValueObjects;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domains.Messaging.GroupEntity;
+public record class GroupTbl {
+    [Key]
+    public string GroupId { get; init; } = string.Empty;
+    public string DisplayId { get; set; } = string.Empty;
+
+    public DateTime CreatedAt {  get; init; }
+    public string CreatorId { get; set; }
+
+    public string Pictures { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+
+
+    public LinkedList<Admin> Admins { get; set; } = new();
+
+    public LinkedList<Member> Members { get; set; } = new();
+    public byte[] Timestamp { get; set; }
+
+    // Relationships
+    public AppUser Creator { get; set; }
+   
+}
---
+++ Domains/Domains.Messaging/Shared/ValueObjects/Member.cs
@@ -0,0 +1,25 @@
+﻿using Shared.Extensions;
+
+namespace Domains.Messaging.Shared.ValueObjects;  
+public record Member {
+    public string UserId { get; init; } = String.Empty;
+    public string Pictures { get; init; } = String.Empty;
+    public DateTime MemberAt { get; init; }
+    public bool IsOnline { get; set; } = false;
+
+    public Member()
+    {
+        
+    }
+    public Member(string userId , string pictures , DateTime memberAt) {
+        UserId = userId;
+        Pictures = pictures;
+        MemberAt = memberAt;
+    }
+
+    public static implicit operator string(Member member) 
+        => member.ToJson();
+    public static implicit operator Member(string jsonSource) 
+        => jsonSource.FromJsonTo<Member>() ?? new Member();
+
+}
