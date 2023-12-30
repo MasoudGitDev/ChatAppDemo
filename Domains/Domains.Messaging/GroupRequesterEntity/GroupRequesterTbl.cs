d2207d4d9b7e7fec60d889b91b0573c2f9c1ce89
MasoudSh <Msh.Git.Dev@gmail.com>
2023-12-30 16:10:39 +0330

    create :  GroupRequesterTbl Entity BlockMemberInfo valueObj

---
+++ Domains/Domains.Messaging/GroupRequesterEntity/GroupRequesterTbl.cs
@@ -0,0 +1,24 @@
+﻿using Domains.Auth.UserEntity;
+using Domains.Messaging.GroupEntity;
+using Shared.Generics;
+using Shared.ValueObjects;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domains.Messaging.GroupRequesterEntity;
+public record class GroupRequesterTbl :IEntity {
+
+    // Ids
+    [Key]
+    public EntityId Id { get; set; }
+    public EntityId RequesterId { get; set; }
+    public EntityId GroupId { get; set; }
+
+    // otherProps
+    public uint RequestNumbers { get; set; } = 0;
+
+    // Relation ships
+    public AppUser Requester { get; set; }
+    public GroupTbl Group { get; set; }
+
+
+}
---
+++ Domains/Domains.Messaging/Shared/ValueObjects/BlockMemberInfo.cs
@@ -0,0 +1,25 @@
+﻿using Shared.Abstractions.Messaging.Constants;
+using Shared.Extensions;
+using Shared.ValueObjects;
+
+namespace Domains.Messaging.Shared.ValueObjects;
+public class BlockMemberInfo {
+
+    public DateTime BlockedAt { get; set; }
+    public EntityId BlockedBy { get; set; }
+    public string? Description { get; set; }
+
+    public BlockMemberInfo() { }
+
+    public BlockMemberInfo(DateTime blockedAt , EntityId blockedBy , string? description) {
+        BlockedAt = blockedAt;
+        BlockedBy = blockedBy;
+        Description = description;
+    }
+
+    public static implicit operator string(BlockMemberInfo groupAdmin)
+        => groupAdmin.ToJson();
+    public static implicit operator BlockMemberInfo(string jsonSource)
+        => jsonSource.FromJsonTo<BlockMemberInfo>() ?? new BlockMemberInfo();
+
+}
