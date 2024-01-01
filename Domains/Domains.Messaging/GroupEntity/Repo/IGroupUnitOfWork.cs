9bb933f029f9048eb5c3dea7aff25c6963ccd238
MasoudSh <Msh.Git.Dev@gmail.com>
2024-01-01 21:55:12 +0330

    move : IGroupUnitOfWork to Shared

--- Domains/Domains.Messaging/GroupEntity/Repo/IGroupUnitOfWork.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Domains.Messaging.GroupEntity.Repo;  
-public interface IGroupUnitOfWork {
-    public IGroupRepo GroupRepo { get;}
-    public IUpdateGroupRepo UpdateRepo { get;}
-
-}
---
+++ Domains/Domains.Messaging/Shared/UnitOfWorks/IGroupUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using Domains.Messaging.GroupEntity.Repo;
+using Domains.Messaging.GroupMemberEntity.Repos;
+
+namespace Domains.Messaging.Shared.UnitOfWorks;  
+public interface IGroupUnitOfWork {
+    public IGroupRepo GroupRepo { get;}
+    public IGroupAdminRepo AdminRepo { get; }
+    public IGroupMemberRepo MemberRepo { get;}
+}
