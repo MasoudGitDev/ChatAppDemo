a6230bf22c6f97371e24685793eb60f7489be1b3
MasoudSh <Msh.Git.Dev@gmail.com>
2024-01-04 22:19:32 +0330

    del : GroupAdminMemberRepo,GroupRepo,GroupRequesterRepo,GroupUnitOfWork,UpdateGroupRepo

--- Infrastructures/Infra.EFCore/Repositories/Messaging/GroupUnitOfWork.cs
+++
@@ -1,8 +0,0 @@
-﻿using Domains.Messaging.GroupEntity.Repo;
-
-namespace Infra.EFCore.Repositories.Messaging {
-    internal sealed class GroupUnitOfWork(IGroupRepo groupRepo,IUpdateGroupRepo updateRepo) : IGroupUnitOfWork {
-        public IGroupRepo GroupRepo => groupRepo;
-        public IUpdateGroupRepo UpdateRepo => updateRepo;
-    }
-}
