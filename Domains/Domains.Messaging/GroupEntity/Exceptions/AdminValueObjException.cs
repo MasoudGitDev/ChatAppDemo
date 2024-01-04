206cf88ac5e57addb4fc9c45840c6c19e5aeb26f
MasoudSh <Msh.Git.Dev@gmail.com>
2024-01-04 20:55:53 +0330

    del : AdminValueObjException,GroupInfoUpdateModel

--- Domains/Domains.Messaging/GroupEntity/Exceptions/AdminValueObjException.cs
+++
@@ -1,9 +0,0 @@
-﻿using Shared.Exceptions;
-
-namespace Domains.Messaging.Shared.Exceptions;
-internal class GroupIdValueObjException : CustomException
-{
-    public GroupIdValueObjException(string where, string code, string description) : base(where, code, description)
-    {
-    }
-}
