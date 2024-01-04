103f4b9184668f63b26a05a3c03fa10fc493f329
MasoudSh <Msh.Git.Dev@gmail.com>
2024-01-04 22:20:58 +0330

    create : GroupMemberId , |Del : IGroupAdminMemberReo,IGroupMemberRepo

---
+++ Domains/Domains.Messaging/GroupMemberEntity/ValueObjects/GroupMemberId.cs
@@ -0,0 +1,7 @@
+﻿using Shared.ValueObjects;
+namespace Domains.Messaging.GroupMemberEntity.ValueObjects {
+    public record GroupMemberId : EntityId {
+        public GroupMemberId(Guid id) : base(id , "GroupMembers") {}
+        public static implicit operator GroupMemberId(Guid id) => new(id);
+    }
+}
