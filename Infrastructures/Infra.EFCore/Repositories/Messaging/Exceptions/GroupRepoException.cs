efa122eafe852db2621ac42a691821d532afcbea
MasoudSh <Msh.Git.Dev@gmail.com>
2024-01-17 11:29:40 +0330

    remvoe : GroupRepoExtention and TryCatchAtrribute

--- Infrastructures/Infra.EFCore/Repositories/Messaging/Exceptions/GroupRepoException.cs
+++
@@ -1,7 +0,0 @@
-﻿using Shared.Exceptions;
-
-namespace Infra.EFCore.Repositories.Messaging.Exceptions;  
-internal class GroupRepoException : CustomException {
-    public GroupRepoException(string where , string code , string description) : base(where , code , description) {
-    }
-}
