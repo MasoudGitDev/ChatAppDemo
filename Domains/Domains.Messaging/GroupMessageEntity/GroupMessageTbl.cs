95f5af8a36d252be50bea0848aacda437106ff10
MasoudSh <Msh.Git.Dev@gmail.com>
2024-01-17 11:47:59 +0330

    create: GroupMessageTbl and its id

---
+++ Domains/Domains.Messaging/GroupMessageEntity/GroupMessageTbl.cs
@@ -0,0 +1,33 @@
+﻿using Domains.Auth.UserEntity;
+using Domains.Messaging.GroupEntity;
+using Domains.Messaging.GroupEntity.ValueObjects;
+using Domains.Messaging.GroupMessageEntity.ValueObjects;
+using Shared.Generics;
+using Shared.ValueObjects;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domains.Messaging.GroupMessageEntity;
+
+[Table("GroupMessages")]
+public record class GroupMessageTbl :IEntity {
+    //ids
+    [Key]
+    public GroupMessageId Id { get; init; }
+    public EntityId AppUserId { get; set; }
+    public GroupId GroupId { get; set; }
+
+    // message info
+    public string Message { get; set; }
+    public bool FirstChecked { get; set; }
+    public bool LastChecked { get; set; }
+    public string? FilePath { get; set; }
+
+
+    public byte[] Timestamp { get; set; }
+
+    // Relationships
+    public AppUser AppUser { get; set; }
+    public GroupTbl Group { get; set; }
+
+}
---
+++ Domains/Domains.Messaging/GroupMessageEntity/ValueObjects/GroupMessageId.cs
@@ -0,0 +1,19 @@
+﻿using Shared.ValueObjects.Exceptions;
+
+namespace Domains.Messaging.GroupMessageEntity.ValueObjects;  
+public record GroupMessageId {
+    public Guid Value { get; set; }
+    public GroupMessageId()
+    {
+        Value = Guid.NewGuid();
+    }
+    public GroupMessageId(Guid id)
+    {
+        if(String.IsNullOrWhiteSpace(id.ToString()) || id == Guid.Empty) {
+            throw new EntityIdException("GroupMessageId-Constructor" , "NotValidGUID" , "");
+        }
+        Value = id;
+    }
+    public static implicit operator GroupMessageId(Guid id) => new GroupMessageId(id);
+    public static implicit operator Guid(GroupMessageId id) => id.Value;
+}
