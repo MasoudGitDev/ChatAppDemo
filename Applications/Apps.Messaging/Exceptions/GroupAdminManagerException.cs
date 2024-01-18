17b670e3309449759d7bbfe3c161b6be2e32f877
MasoudSh <Msh.Git.Dev@gmail.com>
2024-01-18 18:35:07 +0330

    create : GroupRequestManager GroupAdminManager

---
+++ Applications/Apps.Messaging/Exceptions/GroupAdminManagerException.cs
@@ -0,0 +1,7 @@
+﻿using Shared.Exceptions;
+namespace Apps.Messaging.Exceptions {
+    internal class GroupAdminManagerException : CustomException {
+        public GroupAdminManagerException(string where , string code , string description) : base(where , code , description) {
+        }
+    }
+}
---
+++ Presentations/Server.WebAPI/Controllers/Messaging/Exceptions/GroupControllerException.cs
@@ -0,0 +1,8 @@
+﻿using Shared.Exceptions;
+
+namespace Server.WebAPI.Controllers.Messaging.Exceptions {
+    public class GroupControllerException : CustomException {
+        public GroupControllerException(string where , string code , string description) : base(where , code , description) {
+        }
+    }
+}
