c5c04fd1de4b813a911b535521be15664add5c1f
MasoudSh <Msh.Git.Dev@gmail.com>
2024-01-28 14:59:13 +0330

    create : GroupRequestHandler base on partial class

---
+++ Applications/Apps.Messaging/GroupRequests/Manager/GroupRequestHandler_CRUD.cs
@@ -0,0 +1,30 @@
+﻿using Domains.Messaging.GroupEntity.ValueObjects;
+using Domains.Messaging.GroupRequestEntity;
+using Domains.Messaging.GroupRequestEntity.ValueObjects;
+using Domains.Messaging.Shared.ValueObjects;
+
+namespace Apps.Messaging.Managers;
+
+// GroupRequest CRUD
+internal abstract partial class GroupRequestHandler<T,R>{
+    protected async Task CreateAsync(Guid groupId , Guid requesterId , string? description)
+        => await groupRequestRepo.Commands.CreateAsync(new GroupRequestTbl() {
+            GroupId = new GroupId(groupId) ,
+            RequesterId = new AppUserId(requesterId) ,
+            Id = new GroupRequestId() ,
+            RequestNumbers = 1 ,
+            IsBlocked = false ,
+            RequestedAt = DateTime.UtcNow ,
+            Description = description ,
+        });
+
+    protected async Task UpdateAsync(string? description , GroupRequestTbl groupRequest) {
+        CheckDescriptionValue(description , groupRequest.Description);
+        groupRequest.RequestNumbers = groupRequest.RequestNumbers + 1;
+        groupRequest.Description = description;
+        await groupRequestRepo.Commands.UpdateAsync(groupRequest);
+    }
+
+    protected async Task DeleteAsync(GroupRequestTbl groupRequest)
+        => await groupRequestRepo.Commands.DeleteAsync(groupRequest); 
+}
---
+++ Applications/Apps.Messaging/GroupRequests/Manager/GroupRequestHandler_Checking.cs
@@ -0,0 +1,28 @@
+﻿using Domains.Messaging.GroupEntity.ValueObjects;
+using Domains.Messaging.GroupRequestEntity;
+using Domains.Messaging.Shared.ValueObjects;
+using Shared.Exceptions;
+using Shared.Extensions;
+using Shared.Models;
+
+namespace Apps.Messaging.Managers;
+
+// GroupRequest Checking
+internal abstract partial class GroupRequestHandler<T, R> {
+    protected async Task<GroupRequestTbl> GetRequestWithCheckingAsync(GroupId groupId , AppUserId requesterId) {
+        return ( await GetRequestAsync(groupId , requesterId) ).CheckNullability(
+            new ExceptionModel("GetRequestAsync" , "NotFound" , $"The request with groupId = {groupId} and requesterId = {requesterId} was not found"));
+    }
+
+    protected void CheckDescriptionValue(string? description , string? source) {
+        if(string.IsNullOrWhiteSpace(description)) {
+            throw new CustomException("GroupRequestHandler.CheckDescriptionValue" , "NullOrWhiteSpace" , "The <description> must be not null or white space.");
+        }
+        if(description.Trim().Length <= 2) {
+            throw new CustomException("GroupRequestHandler.CheckDescriptionValue" , "LengthError" , "The <description> length must be > 2 characters.");
+        }
+        if(source == description) {
+            throw new CustomException("GroupRequestHandler.CheckDescriptionValue" , "EqualityError" , "The <description> must be differ than <source>.");
+        }
+    }
+}
