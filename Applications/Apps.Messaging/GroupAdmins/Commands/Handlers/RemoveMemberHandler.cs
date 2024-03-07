93abb8358b1cc48a9c85ec527685e7c222320d9c
MasoudSh <Msh.Git.Dev@gmail.com>
2024-03-07 11:28:04 +0330

    update : targetMember and adminLevel checking has been changed

--- Applications/Apps.Messaging/GroupAdmins/Commands/Handlers/RemoveMemberHandler.cs
+++ Applications/Apps.Messaging/GroupAdmins/Commands/Handlers/RemoveMemberHandler.cs
@@ -2,28 +2,32 @@
 using Apps.Messaging.Shared.Manager;
 using Domains.Messaging.GroupEntity.ValueObjects;
 using Domains.Messaging.GroupMemberEntity.Entity;
+using Domains.Messaging.Shared.ValueObjects;
 using Domains.Messaging.UnitOfWorks;
 using Shared.Abstractions.Messaging.Constants;
 using Shared.Exceptions;
 using Shared.Extensions;
 using Shared.Models;
 namespace Apps.Messaging.GroupAdmins.Commands.Handlers;
+
+/// <summary>
+/// Remove and leave method have different approaches. with Remove , no one can remove itself! 
+/// </summary>
 internal sealed class RemoveMemberHandler(IGroupMessagingUOW _unitOfWork)
     : GroupManager<RemoveMemberModel , Result>(_unitOfWork.ThrowIfNull()) {
     public override async Task<Result> Handle(RemoveMemberModel request , CancellationToken cancellationToken) {
 
-        var targetMember = (await GetAdminMemberAsync(request.GroupId, request.MemberId))
-            .ThrowIfNull($"Not found any target admin members with id :{request.MemberId}");
+        var targetMember = (await GetMemberAsync(request.GroupId, request.MemberId))
+            .ThrowIfNull($"Not found any target members with id :{request.MemberId}");
 
         var admin = (await GetAdminMemberAsync(request.GroupId, request.AdminId))
             .ThrowIfNull("You are not admin!");
 
         var adminLevel = admin.AdminInfo!.AdminLevel;
-        if(adminLevel != AdminLevel.Owner || adminLevel != AdminLevel.Deputy) {
-            throw new NotAccessException("You have not permission to remove any member.");
+        if(adminLevel == AdminLevel.Owner || adminLevel == AdminLevel.Deputy) {
+            return await RemoveAsync(admin , targetMember);            
         }
-
-        return await RemoveAsync(admin , targetMember);
+        throw new NotAccessException("You have not permission to remove any member.");
     }
 
     private ResultMessage CreateResultMessage(GroupId groupId , Guid memberId)
@@ -42,5 +46,13 @@ internal sealed class RemoveMemberHandler(IGroupMessagingUOW _unitOfWork)
           } ,
           levelToAssign: null
       );
+    }
+
+    protected override StrategyResult CheckUserIdsEquality(GroupMemberTbl admin , AppUserId targetMemberId) {
+        if(admin.MemberId.Value == targetMemberId.Value) {
+            throw new NotPossibleException(
+                "You can not remove yourself, if you want to do so , you must use the leave method.");
+        }
+        return StrategyResult.NotSameIds;
     }
 }
